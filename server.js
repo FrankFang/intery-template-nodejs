36655378b0f9bd2c6dfcccd0a0d039b12dda6294
frankfang <frankfang1990@gmail.com>
2022-01-24 03:56:26 +0800

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs')
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('你好，世界！');
+}
+
+const server = http.createServer(requestListener);
+
+const socket = process.env.SOCKET
+if (!socket) {
+  console.log("未指定 socket")
+  process.exit(1)
+}
+server.listen(socket, () => {
+  console.log('I am listening on ' + socket)
+});
